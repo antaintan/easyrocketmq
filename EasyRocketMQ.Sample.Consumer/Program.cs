fb13546813120c52ed29273857732b4a1fcf3ac2
antain tan <antaintan@gmail.com>
2017-10-16 15:18:47 +0800

    Update Program.cs

--- EasyRocketMQ.Sample.Consumer/Program.cs
+++ EasyRocketMQ.Sample.Consumer/Program.cs
@@ -7,13 +7,13 @@ namespace EasyRocketMQ.Sample.Consumer
     internal class Program
     {
         // 以下内容换成自己在阿里云的相关信息
-        private static readonly string AccessKeyId = "LTAIspikO0gRSqC5";
+        private static readonly string AccessKeyId = "xxxxxxxxxxxx";
 
-        private static readonly string AccessKeySecret = "Y3l9MVqQsdM2OnOTs7v5dalcPNZXGJ";
+        private static readonly string AccessKeySecret = "xxxxxxxxxxxx";
 
-        private static readonly string Topic = "testpay";
+        private static readonly string Topic = "xxxxxxxxxxxx";
 
-        private static readonly string ConsumerId = "CID_testpay_consumer1";
+        private static readonly string ConsumerId = "xxxxxxxxxxxx";
 
         /// <summary>
         /// 静态全局变量，保持只一个消费者TCP连接
